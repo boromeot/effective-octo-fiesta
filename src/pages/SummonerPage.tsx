ffbb72a84e15c96782463ea73821ca2731b40cd4
boromeot <boromeotyler@gmail.com>
2023-03-14 11:58:13 -0700

    added profile header and icon

--- src/pages/SummonerPage.tsx
+++ src/pages/SummonerPage.tsx
@@ -4,26 +4,39 @@ import MatchHistory from "../ components/MatchHistory";
 
 const SummonerPage = () => {
   return (
-    <div className="grid-container">
-      <div className="grid-item inner-grid-left">
-        <div className='grid-item-left'>left 1</div>
-        <div className='grid-item-left'>left 2</div>
-        <div className='grid-item-left'>left 3</div>
-        <div className='grid-item-left'>left 4</div>
-        <div className='grid-item-left'>left 5</div>
-      </div>
-      <div className="grid-item inner-grid-right">
-        <div className='matchHistory'>item 1</div>
-        <div className='matchHistory'>item 2</div>
-        <MatchHistory />
-        <MatchHistory />
-        <MatchHistory />
-        <MatchHistory />
-        <MatchHistory />
-        <MatchHistory />
-        <MatchHistory />
-      </div>
-    </div>
+    <>
+      <header className='content-header'>
+        <section className='profile'>
+          <div className='profile-icon'>
+            <img src='https://picsum.photos/id/65/4912/3264' alt='profile summoner icon'/>
+          </div>
+          <div className='profile-info'></div>
+        </section>
+        <nav>
+
+        </nav>
+      </header>
+      <main className="grid-container">
+        <div className="grid-item inner-grid-left">
+          <div className='grid-item-left'>left 1</div>
+          <div className='grid-item-left'>left 2</div>
+          <div className='grid-item-left'>left 3</div>
+          <div className='grid-item-left'>left 4</div>
+          <div className='grid-item-left'>left 5</div>
+        </div>
+        <div className="grid-item inner-grid-right">
+          <div className='matchHistory'>item 1</div>
+          <div className='matchHistory'>item 2</div>
+          <MatchHistory />
+          <MatchHistory />
+          <MatchHistory />
+          <MatchHistory />
+          <MatchHistory />
+          <MatchHistory />
+          <MatchHistory />
+        </div>
+      </main>
+    </>
 
   )
 }
