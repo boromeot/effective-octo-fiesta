14375780004345120deb6efdc69978ff996c7b96
boromeot <boromeotyler@gmail.com>
2023-03-15 20:00:04 -0700

    major refactor of summoner page

--- src/pages/SummonerPage.tsx
+++ src/pages/SummonerPage.tsx
@@ -5,88 +5,49 @@ import MatchHistory from "../ components/MatchHistory";
 const SummonerPage = () => {
   return (
     <>
-      <header className='content-header'>
-        <div className='wrapper'>
-          <form className='header-search'>
-            <input className='header-searchbar' type="text" placeholder='Search Summoner'/>
-            <button type="submit">Search</button>
-          </form>
-        </div>
-        <div className='wrapper'>
-          <section className='profile'>
-            <div className='profile-icon'>
-              <img src='https://picsum.photos/id/65/4912/3264' alt='profile summoner icon'/>
-            </div>
-            <div className='profile-info'>
-              <ul className='profile-seasons'>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; Master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; Master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; master
-                </li>
-                <li className='profile-season'>
-                  <b>S2022</b> &nbsp; master
-                </li>
-                
-              </ul>
-              <div className='profile-name'>
-                Oxum 
-              </div>
-              <div className='profile-ladderrank'>
-                Ladder Rank&nbsp; 
-                <a href='#'>1,115</a>&nbsp;
-                (0.124% of top)
-              </div>
-              <button className='profile-update'>
-                Update
-              </button>
-              <div className='profile-lastupdated'>
-                Last updated: 2 hours ago
-              </div>
-            </div>
-          </section>
-        </div>
-        <div className='divider' />
-        <div className='wrapper'>
-          <nav className='profile-nav'>
-            <button><b>Summary</b></button>
-          </nav>
+      <header className='profile-header'>
+        <a href='#'><img src='' alt='Logo' /></a>
+        <input className='summoner-search' type='text' />
+        <div className='mode-toggle'>
+          <div className='light-mode'></div>
+          <div className="divider"></div>
+          <div className='dark-mode'></div>
         </div>
       </header>
-      <main className="grid-container">
-        <div className="grid-item inner-grid-left">
-          <div className='grid-item-left'>left 1</div>
-          <div className='grid-item-left'>left 2</div>
-          <div className='grid-item-left'>left 3</div>
-          <div className='grid-item-left'>left 4</div>
-          <div className='grid-item-left'>left 5</div>
-        </div>
-        <div className="grid-item inner-grid-right">
-          <div className='matchHistory'>item 1</div>
-          <div className='matchHistory'>item 2</div>
-        </div>
-      </main>
-    </>
 
+
+      <main className=''>
+        <section>
+          <div className="container">
+            <ul className='previous-ranks'>
+              <li>S12 Master</li>
+              <li>S11 Diamond</li>
+              <li>S10 Diamond</li>
+              <li>S9 Diamond</li>
+              <li>S8 Diamond</li>
+              <li>S7 Platinum</li>
+              <li>S6 Platinum</li>
+            </ul>
+            <div className='profile-info'>
+              <img src="" alt="summoner icon" />
+              <div>
+                <div>Name</div>
+                <div>Ladder Rank</div>
+                <button>Update</button>
+              </div>
+            </div>
+            <nav className='profile-nav'>
+              <a href="#">Overview</a>
+            </nav>
+          </div>
+        </section>
+
+
+        <section></section>
+      </main>
+
+
+    </>
   )
 }
 
