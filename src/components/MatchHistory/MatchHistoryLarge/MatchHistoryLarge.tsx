8388be4da77df2869f05fc12d3270837c027beff
boromeot <boromeotyler@gmail.com>
2023-03-27 21:44:15 -0700

    auto formmated document

--- src/components/MatchHistory/MatchHistoryLarge/MatchHistoryLarge.tsx
+++ src/components/MatchHistory/MatchHistoryLarge/MatchHistoryLarge.tsx
@@ -41,34 +41,34 @@ function timeConverter(unixTimeStamp: number) {
 
   const diff = Date.now() - new Date(unixTimeStamp).valueOf();
   const time = formatter.format(Math.round(-diff / (1000 * 60 * 60 * 24)), 'days');
-  
+
   return time;
 }
 
-function MatchHistoryLarge({ info, metaData } : MatchData) {
+function MatchHistoryLarge({ info, metaData }: MatchData) {
   return (
     <div className="matchHistory-large">
       <div className="col-1">
-  
+
         <div className="row-1">
           <div><b>Ranked Solo</b></div>
-          <div>{ timeConverter(info.gameCreation) }</div>
+          <div>{timeConverter(info.gameCreation)}</div>
         </div>
-        
+
         <div className="row-2">
           25 LP
         </div>
-  
+
         <div className="row-3">
           <span>WIN</span>
           &nbsp;
-          <span>{ secondsToMinuteSeconds(info.gameDuration) }</span>
+          <span>{secondsToMinuteSeconds(info.gameDuration)}</span>
         </div>
-  
+
       </div>
       <div className="col-2">
         <div className="champion">
-          <img src="https://picsum.photos/id/34/3872/2592"/>
+          <img src="https://picsum.photos/id/34/3872/2592" />
           <div className="level">16</div>
         </div>
         <div className="summoner-spell">
@@ -104,18 +104,18 @@ function MatchHistoryLarge({ info, metaData } : MatchData) {
       <div className="col-5">
         <div className="teamlist">
           {
-            info && info.participants.slice(0, info.participants.length / 2).map( participant  => {
+            info && info.participants.slice(0, info.participants.length / 2).map(participant => {
               return (
-                <Summoner src="https://picsum.photos/id/35/1280/901" summonerName={participant.summonerName} key={participant.summonerId}/>
+                <Summoner src="https://picsum.photos/id/35/1280/901" summonerName={participant.summonerName} key={participant.summonerId} />
               )
             })
           }
         </div>
         <div className="teamlist">
-        {
-            info && info.participants.slice(info.participants.length / 2).map( participant  => {
+          {
+            info && info.participants.slice(info.participants.length / 2).map(participant => {
               return (
-                <Summoner src="https://picsum.photos/id/35/1280/901" summonerName={participant.summonerName} key={participant.summonerId}/>
+                <Summoner src="https://picsum.photos/id/35/1280/901" summonerName={participant.summonerName} key={participant.summonerId} />
               )
             })
           }
@@ -123,22 +123,22 @@ function MatchHistoryLarge({ info, metaData } : MatchData) {
       </div>
     </div>
   )
-} 
+}
 
-function Summoner({ src, summonerName }: {src : string, summonerName : string}) {
+function Summoner({ src, summonerName }: { src: string, summonerName: string }) {
   return (
     <div className="summoner">
-      <img src={ src } />
-      <a>{ summonerName }</a>
+      <img src={src} />
+      <a>{summonerName}</a>
     </div>
   )
 }
 
-function Item({ src }: {src : string | undefined}) {
+function Item({ src }: { src: string | undefined }) {
   return (
     <div className="item-container">
       {
-        src && <img className='item' src={ src }/>
+        src && <img className='item' src={src} />
       }
     </div>
   )
